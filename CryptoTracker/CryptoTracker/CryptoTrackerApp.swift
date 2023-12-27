6c2e11301dfceab84bc3fa4eb344de78d8da13d9
Mykyta Kuzminov <kuzminov.mykyta26@icloud.com>
2023-12-27 12:04:38 +0200

    Set up a project

---
+++ CryptoTracker/CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Микита Кузьмінов on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
