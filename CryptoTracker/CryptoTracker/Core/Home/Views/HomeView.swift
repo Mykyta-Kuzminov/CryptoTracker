c7fd185e6fceeba9e9c1eff2d79696eab5363e67
Mykyta Kuzminov <kuzminov.mykyta26@icloud.com>
2023-12-29 09:58:34 +0200

    Designed a dynamic List of coins

--- CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/CryptoTracker/Core/Home/Views/HomeView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     
+    @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     
     var body: some View {
@@ -20,6 +21,16 @@ struct HomeView: View {
             VStack {
                 homeHeader
                 
+                columnTitles
+                
+                if !showPortfolio {
+                    allCoinsList
+                }
+                
+                if showPortfolio {
+                    portfolioCoinsList
+                }
+                
                 Spacer(minLength: 0)
             }
         }
@@ -30,6 +41,7 @@ struct HomeView: View {
     NavigationStack {
         HomeView()
             .toolbar(.hidden)
+            .environmentObject(DeveloperPreview.instance.vm)
     }
 }
 
@@ -63,4 +75,40 @@ extension HomeView {
         .padding(.horizontal)
     }
     
+    private var allCoinsList: some View {
+        List {
+            ForEach(vm.allCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: false)
+            }
+        }
+        .listStyle(.plain)
+        .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+        .transition(.move(edge: .leading))
+    }
+    
+    private var portfolioCoinsList: some View {
+        List {
+            ForEach(vm.portfolioCoins) { coin in
+                CoinRowView(coin: coin, showHoldingsColumn: true)
+            }
+        }
+        .listStyle(.plain)
+        .listRowInsets(.init(top: 10, leading: 0, bottom: 10, trailing: 10))
+        .transition(.move(edge: .trailing))
+    }
+    
+    private var columnTitles: some View {
+        HStack {
+            Text("Coin")
+            Spacer()
+            if showPortfolio {
+                Text("Holdings")
+            }
+            Text("Price")
+                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+        }
+        .font(.caption)
+        .foregroundStyle(Color.secondaryText)
+        .padding(.horizontal)
+    }
 }
